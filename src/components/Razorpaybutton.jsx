ffa2f60457b0bfa021ed2d35ff8e4217118617f8
ayush singh <ayushsingh16022006@gmail.com>
2026-04-12 23:14:57 +0530

    Point frontend to Render backend

--- src/components/Razorpaybutton.jsx
+++ src/components/Razorpaybutton.jsx
@@ -120,3 +120,5 @@ export default function RazorpayButton({ bookingId, amount, onSuccess, onFailure
 
 
 
+
+
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -80,3 +80,5 @@ export default function Sidebar({ role = 'customer', activeTab, onTabChange }) {
 
 
 
+
+
